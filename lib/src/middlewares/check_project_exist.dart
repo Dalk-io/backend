e392c2b5d0a4837ac0e4745c5c42378578959639
Kevin Segaud <kevin@santetis.fr>
2020-04-20 22:40:05 +0200

    add some log into the middleware
    Signed-off-by: Kevin Segaud <kevin@santetis.fr>

--- lib/src/middlewares/check_project_exist.dart
+++ lib/src/middlewares/check_project_exist.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
 
 import 'package:backend/src/rpc/project/get_project_by_key.dart';
+import 'package:logging/logging.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 Middleware checkProjectExistMiddleware(GetProjectByKey getProjectByKey) => (Handler handler) {
+      final logger = Logger('checkProjectExistMiddleware');
       return (request) async {
         final projectId = params(request, 'id');
+        logger.info(projectId);
         final project = await getProjectByKey.request(projectId);
+        logger.info(project);
         if (project == null) {
           return Response.notFound(json.encode({'message': 'Project $projectId not found'}));
         }
