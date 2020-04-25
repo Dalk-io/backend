a4cfcbfb64d36e12c49b8091198102904a041ca2
Kevin Segaud <kevin@santetis.fr>
2020-04-25 16:20:06 +0200

    better way to json serialize message
    Signed-off-by: Kevin Segaud <kevin@santetis.fr>

--- lib/src/utils/message_to_json.dart
+++ lib/src/utils/message_to_json.dart
@@ -1,11 +1,10 @@
 import 'package:backend/src/data/message/message.dart';
 
 Map<String, dynamic> messageToJson(MessageData message, {bool filter = true}) {
+  final cleanedMessage = message.copyWith(projectId: null, conversationId: null);
   return <String, dynamic>{
-    ...message.toJson(),
+    ...cleanedMessage.toJson(),
     'status': computeMessageStatus(message, filter: filter),
     if (filter) 'statusDetails': computeMessageStatusDetails(message),
-  }
-    ..remove('projectId')
-    ..remove('conversationId');
+  };
 }
